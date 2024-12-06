1dba15c7a81f8068fd0ef801048b0a6108d18d69
Udai Chauhan <udaichauhan284@gmail.com>
2024-12-06 18:07:31 +0530

    Add Vercel.json and some changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,10 @@ app.use(express.json()); // For parsing JSON
 app.use("/api/auth/", auth);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 //Server Listening
